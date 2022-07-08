a5baffa1f22f735469bf3b24a77e6f8d99007797
JJ Allaire <jj@rstudio.com>
2022-07-08 10:26:14 -0400

    simplify fontawesome.lua

--- _extensions/fontawesome/fontawesome.lua
+++ _extensions/fontawesome/fontawesome.lua
@@ -1,44 +1,36 @@
 function ensureLatexDeps()
-    quarto.doc.useLatexPackage("fontawesome5")
+  quarto.doc.useLatexPackage("fontawesome5")
 end
 
 function ensureHtmlDeps()
-    quarto.doc.addHtmlDependency({
-        name = 'fontawesome6',
-        version = '0.1.0',
-        scripts = {
-        },
-        stylesheets = {
-            {
-            name = 'assets/css/all.css',
-            path = 'assets/css/all.css'
-            }
-        }
-    })
+  quarto.doc.addHtmlDependency({
+    name = 'fontawesome6',
+    version = '0.1.0',
+    stylesheets = {
+      {
+        name = 'assets/css/all.css',
+        path = 'assets/css/all.css'
+      }
+    }
+  })
 end
 
 return {
-    ["fa"] = function(args, kwargs)
-        local icon = pandoc.utils.stringify(args[1]) -- FIXME ESCAPE
+  ["fa"] = function(args, kwargs)
 
-        if FORMAT == 'docx' then
-            content = ""
-            return pandoc.RawBlock('openxml', "")
-        elseif FORMAT:match 'latex' then
-            ensureLatexDeps()
-            return pandoc.RawBlock('tex', "\\faIcon{" .. icon .. "}")
-        elseif FORMAT:match 'odt' then
-            return pandoc.RawBlock('opendocument', "")
-        elseif FORMAT:match 'html.*' then
-            ensureHtmlDeps()
-            return pandoc.RawBlock('html', "<i class=\"fa-solid fa-" .. icon .. "\"></i>")
-        elseif FORMAT:match 'epub' then
-            return pandoc.RawBlock('html', "")
-        elseif FORMAT:match 'context' then
-            return pandoc.RawBlock('context', "")
-        else
-            -- fall back to insert a span
-            return pandoc.Span(icon)
-        end
+    local icon = pandoc.utils.stringify(args[1])
+
+    -- detect html (excluding epub which won't handle fa)
+    if quarto.doc.isFormat("html:js") then
+      ensureHtmlDeps()
+      return pandoc.RawInline('html', "<i class=\"fa-solid fa-" .. icon .. "\"></i>")
+    -- detect pdf / beamer / latex / etc
+    elseif quarto.doc.isFormat("pdf") then
+      ensureLatexDeps()
+      return pandoc.RawInline('tex', "\\faIcon{" .. icon .. "}") 
+    else
+      return pandoc.Null()
     end
+
+  end
 }
