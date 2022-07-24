942f010eb80ba98eef52df76c8e4b4ee5ee4e6b2
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-07-24 19:26:27 +0200

    feat: allows latex fontsize command

--- _extensions/fontawesome/fontawesome.lua
+++ _extensions/fontawesome/fontawesome.lua
@@ -1,8 +1,8 @@
-function ensureLatexDeps()
+local function ensureLatexDeps()
   quarto.doc.useLatexPackage("fontawesome5")
 end
 
-function ensureHtmlDeps()
+local function ensureHtmlDeps()
   quarto.doc.addHtmlDependency({
     name = 'fontawesome6',
     version = '0.1.0',
@@ -14,6 +14,27 @@ local function isEmpty(s)
   return s == nil or s == ''
 end
 
+local function isValidSize(size)
+  local validSizes = {
+    "tiny",
+    "scriptsize",
+    "footnotesize",
+    "small",
+    "normalsize",
+    "large",
+    "Large",
+    "LARGE",
+    "huge",
+    "Huge"
+  }
+  for _, v in ipairs(validSizes) do
+    if v == size then
+      return size
+    end
+  end
+  return "normalsize"
+end
+
 return {
   ["fa"] = function(args, kwargs)
 
@@ -23,19 +44,20 @@ return {
       group = icon
       icon = pandoc.utils.stringify(args[2])
     end
-    local size = pandoc.utils.stringify(kwargs["size"])
-    if not isempty(size) then
-      size = " fa-" .. size
-    end
     
     local title = pandoc.utils.stringify(kwargs["title"])
-    if not isempty(title) then
+    if not isEmpty(title) then
       title = " title=\"" .. title  .. "\" aria-hidden=\"true\""
     end
+
+    local size = pandoc.utils.stringify(kwargs["size"])
     
     -- detect html (excluding epub which won't handle fa)
     if quarto.doc.isFormat("html:js") then
       ensureHtmlDeps()
+      if not isEmpty(size) then
+        size = " fa-" .. size
+      end
       return pandoc.RawInline(
         'html',
         "<i class=\"fa-" .. group .. " fa-" .. icon .. size .. "\"" .. title .. "></i>"
@@ -43,10 +65,13 @@ return {
     -- detect pdf / beamer / latex / etc
     elseif quarto.doc.isFormat("pdf") then
       ensureLatexDeps()
-      return pandoc.RawInline('tex', "\\faIcon{" .. icon .. "}") 
+      if isEmpty(size) then
+        return pandoc.RawInline('tex', "\\faIcon{" .. icon .. "}")
+      else
+        return pandoc.RawInline('tex', "\\" .. isValidSize(size) .. "{\\faIcon{" .. icon .. "}}")
+      end
     else
       return pandoc.Null()
     end
-
   end
 }
